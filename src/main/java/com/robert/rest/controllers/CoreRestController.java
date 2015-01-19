eab6d6c5a6dccd2de9425bdc3b66047bf1bc78ab
rhutto <robert.hutto@catalinamarketing.com>
2015-01-18 23:09:12 -0500

    added application.properties

--- src/main/java/com/robert/rest/controllers/CoreRestController.java
+++ src/main/java/com/robert/rest/controllers/CoreRestController.java
@@ -18,7 +18,7 @@ public class CoreRestController {
     public ResponseEntity<Message> getOffers(@PathVariable String value) {
         Message message = new Message();
         message.setValue(value);
-        return new ResponseEntity<>(messag, HttpStatus.OK);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
 }
